c63f33e3e5a735798576aa122e0b7f4c6d8be1b4
Masafumi Okada <mf@okadajp.org>
2023-05-12 15:48:28 +0900

    M5Stamp C3+ラウンド液晶向け修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <HTTPClient.h>
-#include <M5Unified.h>
+#include <M5Unified.hpp>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
@@ -7,6 +7,80 @@
 
 // include WiFi / MQTT / staticmap settings
 #include "config.h"
+#include "lgfx/v1/panel/Panel_GC9A01.hpp"
+
+// パネル定義 M5Stampラウンド液晶モジュール https://ssci.to/8099 
+// LGFX_M5Stamp_SPI_GC9A01クラス定義は, サンプルプログラム https://github.com/urukakanko/M5Stamp_round_lcd/blob/main/M5stamp_ClockSample.ino より引用
+class LGFX_M5Stamp_SPI_GC9A01 : public lgfx::LGFX_Device
+{
+
+lgfx::Panel_GC9A01 _panel_instance;
+lgfx::Bus_SPI       _bus_instance;   // SPIバスのインスタンス
+lgfx::Light_PWM     _light_instance;
+public:
+  LGFX_M5Stamp_SPI_GC9A01(void)
+  {
+    { // バス制御の設定を行います。
+      auto cfg = _bus_instance.config();    // バス設定用の構造体を取得します。
+
+// SPIバスの設定
+      cfg.spi_host = SPI2_HOST;     // 使用するSPIを選択  ESP32-S2,C3 : SPI2_HOST or SPI3_HOST / ESP32 : VSPI_HOST or HSPI_HOST
+      // ※ ESP-IDFバージョンアップに伴い、VSPI_HOST , HSPI_HOSTの記述は非推奨になるため、エラーが出る場合は代わりにSPI2_HOST , SPI3_HOSTを使用してください。
+      cfg.spi_mode = 0;             // SPI通信モードを設定 (0 ~ 3)
+      cfg.freq_write = 40000000;    // 送信時のSPIクロック (最大80MHz, 80MHzを整数で割った値に丸められます)
+      cfg.freq_read  = 16000000;    // 受信時のSPIクロック
+      cfg.spi_3wire  = true;        // 受信をMOSIピンで行う場合はtrueを設定
+      cfg.use_lock   = true;        // トランザクションロックを使用する場合はtrueを設定
+      cfg.dma_channel = SPI_DMA_CH_AUTO; // 使用するDMAチャンネルを設定 (0=DMA不使用 / 1=1ch / 2=ch / SPI_DMA_CH_AUTO=自動設定)
+      // ※ ESP-IDFバージョンアップに伴い、DMAチャンネルはSPI_DMA_CH_AUTO(自動設定)が推奨になりました。1ch,2chの指定は非推奨になります。
+      cfg.pin_sclk = 4;            // SPIのSCLKピン番号を設定
+      cfg.pin_mosi = 6;            // SPIのMOSIピン番号を設定
+      cfg.pin_miso = -1;            // SPIのMISOピン番号を設定 (-1 = disable)
+      cfg.pin_dc   = 1;            // SPIのD/Cピン番号を設定  (-1 = disable)
+     // SDカードと共通のSPIバスを使う場合、MISOは省略せず必ず設定してください。
+      _bus_instance.config(cfg);    // 設定値をバスに反映します。
+      _panel_instance.setBus(&_bus_instance);      // バスをパネルにセットします。
+    }
+
+    { // 表示パネル制御の設定を行います。
+      auto cfg = _panel_instance.config();    // 表示パネル設定用の構造体を取得します。
+      cfg.pin_cs           =    7;  // CSが接続されているピン番号   (-1 = disable)
+      cfg.pin_rst          =    0;  // RSTが接続されているピン番号  (-1 = disable)
+      cfg.pin_busy         =    -1;  // BUSYが接続されているピン番号 (-1 = disable)
+      // ※ 以下の設定値はパネル毎に一般的な初期値が設定されていますので、不明な項目はコメントアウトして試してみてください。
+      cfg.memory_width     =   240;  // ドライバICがサポートしている最大の幅
+      cfg.memory_height    =   240;  // ドライバICがサポートしている最大の高さ
+      cfg.panel_width      =   240;  // 実際に表示可能な幅
+      cfg.panel_height     =   240;  // 実際に表示可能な高さ
+      cfg.offset_x         =     0;  // パネルのX方向オフセット量
+      cfg.offset_y         =     0;  // パネルのY方向オフセット量
+      cfg.offset_rotation  =     0;  // 回転方向の値のオフセット 0~7 (4~7は上下反転)
+      cfg.dummy_read_pixel =     8;  // ピクセル読出し前のダミーリードのビット数
+      cfg.dummy_read_bits  =     1;  // ピクセル以外のデータ読出し前のダミーリードのビット数
+      cfg.readable         =  true;  // データ読出しが可能な場合 trueに設定
+      cfg.invert           =  true;  // パネルの明暗が反転してしまう場合 trueに設定
+      cfg.rgb_order        = false;  // パネルの赤と青が入れ替わってしまう場合 trueに設定
+      cfg.dlen_16bit       = false;  // データ長を16bit単位で送信するパネルの場合 trueに設定
+      cfg.bus_shared       =  true;  // SDカードとバスを共有している場合 trueに設定(drawJpgFile等でバス制御を行います)
+
+      _panel_instance.config(cfg);
+    }
+    { // バックライト制御の設定を行います。（必要なければ削除）
+      auto cfg = _light_instance.config();    // バックライト設定用の構造体を取得します。
+
+      cfg.pin_bl = 10;              // バックライトが接続されているピン番号
+      cfg.invert = false;           // バックライトの輝度を反転させる場合 true
+      cfg.freq   = 44100;           // バックライトのPWM周波数
+      cfg.pwm_channel = 3;          // 使用するPWMのチャンネル番号
+
+      _light_instance.config(cfg);
+      _panel_instance.setLight(&_light_instance);  // バックライトをパネルにセットします。
+    }
+    setPanel(&_panel_instance); // 使用するパネルをセットします。
+  }
+};
+
+LGFX_M5Stamp_SPI_GC9A01 lcd;
 
 // WiFi client
 WiFiClient wifiClient, wifiClient2;
@@ -81,7 +155,7 @@ void downloadAndDisplayImage(const char* url) {
   }
 
   M5.Log.printf("Image downloaded. Size: %d\n", imageDataIndex);
-  M5.Display.drawJpg(imageData, imageDataIndex, 0, 0);
+  lcd.drawJpg(imageData, imageDataIndex, 0, 0);
 }
 
 void reboot() {
@@ -150,6 +224,7 @@ void setup() {
   M5_LOGI("M5 start");
   imageData = (uint8_t *)malloc(BUFFERSIZE);
   M5_LOGI("LCD start");
+  lcd.init();
   chipid = ESP.getEfuseMac(); // チップIDを取得
   // uint64_t型のchipidを16進数の文字列に変換  
   M5.Log.printf("ESP32 Chip ID = %016llX", chipid);
@@ -157,35 +232,35 @@ void setup() {
   uint32_t upper = chipid >> 32;
   uint32_t lower = (uint32_t) chipid;
   clientID = "M5Unified_" + String(upper, HEX) + String(lower, HEX);
-//  M5.Display.setRotation(1);
-//  M5.Display.setBrightness(20);
-  screen_size = String(M5.Display.width()) + "x" + String(M5.Display.height());
-  M5.Display.setTextWrap(true, true);
-  M5.Display.fillScreen(TFT_BLUE);
-  M5.Display.setTextColor(TFT_WHITE);
-  M5.Display.setFont(&fonts::Font4);
-  M5.Display.println(F("[ImadokoM5U]"));
-  M5.Display.println();
+//  lcd.setRotation(1);
+//  lcd.setBrightness(20);
+  screen_size = String(lcd.width()) + "x" + String(lcd.height());
+  lcd.setTextWrap(true, true);
+  lcd.fillScreen(TFT_BLUE);
+  lcd.setTextColor(TFT_WHITE);
+  lcd.setFont(&fonts::Font4);
+  lcd.println(F("[ImadokoM5U]"));
+  lcd.println();
 
   // Connect to WiFi network
-  M5.Display.println();
-  M5.Display.println();
-  M5.Display.print("Connecting to ");
-  M5.Display.println(ssid);
+  lcd.println();
+  lcd.println();
+  lcd.print("Connecting to ");
+  lcd.println(ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   int count = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    M5.Display.print(".");
+    lcd.print(".");
     if (count++ > 20) {
-      M5.Display.println("Failed to connect to WiFi");
+      lcd.println("Failed to connect to WiFi");
       reboot();
     }
   }
 
-  M5.Display.println("");
-  M5.Display.println("WiFi connected");
+  lcd.println("");
+  lcd.println("WiFi connected");
   
   // Set the MQTT server and callback function
   client.setServer(mqtt_server, mqtt_port);
